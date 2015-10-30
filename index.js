b64fa46fc1fd234e683816039dc4be3d89319e8a
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-10-30 12:03:54 +0100

    add missing transformations

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: [
     require('babel-plugin-transform-es2015-destructuring'),
+    require('babel-plugin-transform-es2015-modules-commonjs'),
+    require('babel-plugin-transform-es2015-parameters'),
+    require('babel-plugin-transform-es2015-sticky-regex'),
+    require('babel-plugin-transform-es2015-unicode-regex'),
   ],
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
-const { expect } = require('chai')
+import { expect } from 'chai'
 
 describe('babel-preset-es2015-node5', () => {
-  it('works', () => {
-    expect(true).equal(true)
+  it('parameters', () => {
+    const arrayOf = (...args) => args
+    const sum = (a = 1, b = a) => a + b
+
+    expect(arrayOf(1, 2, 3)).eql([1, 2, 3])
+    expect(sum()).equal(2)
+  })
+
+  it('regexps', () => {
+    expect('𠮷'.match(/^.$/u)[0].length).equal(2) // eslint-disable-line no-empty-character-class
+    expect('𝌆'.match(/\u{1d306}/u)[0].length).equal(2) // eslint-disable-line no-empty-character-class
   })
 })
