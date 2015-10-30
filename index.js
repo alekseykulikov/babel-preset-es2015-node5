0eaaa5c6c3dfbcc00ae1b9a784bfa587bc482774
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-10-30 11:27:32 +0100

    add simple test suite

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: [
+    require('babel-plugin-transform-es2015-destructuring'),
+  ],
+}
---
+++ test/index.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai')
+
+describe('babel-preset-es2015-node5', () => {
+  it('works', () => {
+    expect(true).equal(true)
+  })
+})
