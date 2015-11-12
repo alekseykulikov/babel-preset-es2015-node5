fcda4361408c2ea2ea2da5fc17610957e1a137e3
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-11-12 14:13:14 +0100

    update deps and tests

--- test/index.js
+++ test/index.js
@@ -4,9 +4,14 @@ describe('babel-preset-es2015-node5', () => {
   it('parameters', () => {
     const arrayOf = (...args) => args
     const sum = (a = 1, b = a) => a + b
+    const pushArray = (a1, a2) => {
+      a1.push(...a2)
+      return a1
+    }
 
     expect(arrayOf(1, 2, 3)).eql([1, 2, 3])
     expect(sum()).equal(2)
+    expect(pushArray([1], [2])).eql([1, 2])
   })
 
   it('regexps', () => {
